f7ed943d70c26e4f5e3201543554495e47709f99
thoidaianhhung <hungnvse00535x@funix.edu.vn>
2024-09-16 17:18:37 +0700

    code ngay 16/9

---
+++ account-service/src/main/java/com/vti/account_service/entity/User.java
@@ -0,0 +1,25 @@
+package com.vti.account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", length = 50, nullable = false, unique = true)
+    private String phone;
+}
